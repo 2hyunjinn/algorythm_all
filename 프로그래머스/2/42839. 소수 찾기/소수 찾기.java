6acee8a709933a254ba9865bbdf7b8ec8d407d9d
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2025-07-07 16:10:32 +0900

    [level 2] Title: 소수 찾기, Time: 10.79 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    private static HashSet<Integer> primeN = new HashSet<>();
+    private static boolean[] visited;
+
+    public static int solution(String numbers) {
+        String[] number = numbers.split("");
+        visited = new boolean[number.length];
+        dfs("", number);
+        return primeN.size();
+    }
+
+    private static void dfs(String current, String[] number) {
+        if(!current.equals("")){
+            if(isPrime(current)) primeN.add(Integer.parseInt(current));
+        }
+
+        for(int i = 0; i < number.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(current+number[i], number);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static boolean isPrime(String num){
+        int n = Integer.parseInt(num);
+
+        if(n <= 1) return false;
+        if(n == 2) return true;
+
+        for(int i = 2; i<=Math.sqrt(n); i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+}
