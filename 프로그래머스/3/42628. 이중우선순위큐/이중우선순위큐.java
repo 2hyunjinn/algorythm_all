a5184920e46a3332232b0c88d7d0f0c2f7c3552c
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2025-08-10 13:05:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 90.45 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,16 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        int[] answer = {};
         TreeSet<Integer> set = new TreeSet<>();
         for(String op : operations){
-            String[] o = op.split(" ");
-            if(o[0].equals("I")) set.add(Integer.parseInt(o[1]));
-            else if(o[0].equals("D")){
-                if(set.isEmpty()) continue;
-                else if(o[1].equals("1")) set.remove(set.last());
+            if(op.split(" ")[0].equals("I")){
+                set.add(Integer.parseInt(op.split(" ")[1]));
+            } else if(op.split(" ")[0].equals("D") && !set.isEmpty()){
+                if(op.split(" ")[1].equals("1")) set.remove(set.last());
                 else set.remove(set.first());
-            }                     
+            }
         }
+        
         return (set.isEmpty())? new int[]{0,0} : new int[]{set.last(), set.first()};
     }
 }
