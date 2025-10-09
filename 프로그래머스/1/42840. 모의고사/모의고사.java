e12ada5c1ae8466dc7c35405442e5ab6688e03d2
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2025-10-10 03:23:14 +0900

    [level 1] Title: 모의고사, Time: 2.79 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = new int[]{1,2,3,4,5};
-        int[] two = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        Map<Integer, Integer> map = new HashMap<>();
         
-        int cnt1 = 0; int cnt2 = 0; int cnt3 = 0;
+        int[] one = new int[]{1,2,3,4,5};
+        int[] two = new int[]{2,1,2,3,2,4,2,5};
+        int[] three = new int[]{3,3,1,1,2,2,4,4,5,5};
+        
         for(int i = 0; i<answers.length; i++){
-            int nowA = answers[i];
-            int now1 = one[i%5];
-            int now2 = two[i%8];
-            int now3 = three[i%10];
-            
-            if(nowA == now1) cnt1++;
-            if(nowA == now2) cnt2++;
-            if(nowA == now3) cnt3++;
+            int ans = answers[i];
+            if(one[i%one.length] == ans) map.put(1, map.getOrDefault(1, 0) + 1);
+            if(two[i%two.length] == ans) map.put(2, map.getOrDefault(2, 0) + 1);
+            if(three[i%three.length] == ans) map.put(3, map.getOrDefault(3, 0) + 1);
         }
         
-        int maxV = Math.max(cnt1, Math.max(cnt2, cnt3));
-        int idx = 0;
-        List<Integer> list = new LinkedList<>();
+        int maxV = 0;
+        for(int v : map.values()){
+            maxV = Math.max(maxV, v);
+        }
         
-        if(cnt1 == maxV) list.add(1);
-        if(cnt2 == maxV) list.add(2);
-        if(cnt3 == maxV) list.add(3);
+        List<Integer> list = new LinkedList<>();
+        for(int key : map.keySet()){
+            if(map.get(key) == maxV) list.add(key);
+        }
         
         int[] answer = new int[list.size()];
-        for(int i = 0; i<list.size(); i++){
+        for(int i = 0; i<answer.length; i++){
             answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
